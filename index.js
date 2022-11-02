a3b8d550b165aa9eafe6b3c553f2adbae8282a84
Solomon Adegboyega <solomonadegboyega1@gmail.com>
2022-11-02 13:43:58 +0100

    PORT updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const catRoute = require("./routes/categories");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8800;
 
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
